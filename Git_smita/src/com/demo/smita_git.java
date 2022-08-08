41188d5ce40e747d5b2d201fd0e77ff96fb842c9
vansh <pawarsmita12345678@gmail.com>
2022-08-08 15:07:43 +0530

    By smita

---
+++ Git_smita/src/com/demo/smita_git.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class smita_git
+{
+	public void m1()
+	{
+		System.out.println("M1------------method");
+	}
+	public void m2()
+	{
+		System.out.println("m2-------------method ");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("smita pawar");
+		
+	}
+
+}
